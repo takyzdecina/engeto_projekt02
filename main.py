8da043a012ead0d9fa902988f191fa669e184421
takyzdecina <smidjakub13@gmail.com>
2025-05-26 18:18:20 +0200

    první verze

---
+++ main.py
@@ -0,0 +1,96 @@
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+
+author: Jakub Šmíd
+email: smidjakub13@gmail.com
+"""
+
+from random import sample
+
+
+def vygeneruje_zadani(pocet_cislic) -> list:
+    """generuje hádaný kód o daném počtu unikátních znaků (max 9)"""
+
+    zadani = sample(range(1, 9), pocet_cislic)
+    return zadani
+
+
+def vyzada_cisla_uzivatele(pocet_cislic) -> list:
+    """ošetřuje zadání od uživatele - vyžaduje daný počet unikátních číselných znaků"""
+
+    while True:
+        zadane_cislo = input(">>> ")
+        if zadane_cislo.isdigit() and len(zadane_cislo) == pocet_cislic == len(
+            set(zadane_cislo)
+        ):
+            zpracovane_zadane_cislo = [int(i) for i in zadane_cislo]
+            break
+        else:
+            print(f"It must be {pocet_cislic} unique digits.")
+    return zpracovane_zadane_cislo
+
+
+def kontrola_bulls_and_cows(
+    cisla_od_uzivatele: list, 
+    cisla_od_pocitace: list, 
+    pocet_cislic: int
+) -> dict:
+    
+    """Zapisuje shody úplné i částečné"""
+    dobytek = {"cow": 0, "bull": 0}
+    for i in range(pocet_cislic):
+        if cisla_od_uzivatele[i] == cisla_od_pocitace[i]:
+            dobytek["bull"] += 1
+        elif cisla_od_uzivatele[i] in cisla_od_pocitace:
+            dobytek["cow"] += 1
+
+    return dobytek
+
+
+def sumarizace_vysledku(dobytek: dict) -> bool:
+    """Zpracuje výstup z kontrola_bulls_and_cows a vypíše vysledek, 
+    nebo iniciuje pogratulování."""
+
+    if dobytek["bull"] == 4:
+        print(oddelovac)
+        return True
+
+    else:
+        bull_s = "bulls" if dobytek["bull"] != 1 else "bull"
+        cow_s = "cows" if dobytek["cow"] != 1 else "cow"
+        print(
+            f"{dobytek['bull']} {bull_s}, " f"{dobytek['cow']} {cow_s}\n" f"{oddelovac}"
+        )
+        return False
+
+# neměnné start
+oddelovac = 47 * "-"
+vytezstvi = False
+pocet_kol = 0
+# neměnné konec
+
+pocet_cislic = 4 # proměnná určující počet číslic v zadání. Max 9.
+
+
+print(
+    f"Hi there!\n{oddelovac}\n"
+    "I've generated a random 4 digit number for you.\n"
+    f"Let's play a bulls and cows game.\n{oddelovac}"
+)
+
+cisla_pocitace = vygeneruje_zadani(pocet_cislic)
+
+print(f"Enter {pocet_cislic} numbers:")
+
+while not vytezstvi:
+    """ústřední část"""
+
+    pocet_kol += 1  # započítá započaté kolo
+    cisla_uzivatele = vyzada_cisla_uzivatele(pocet_cislic)
+    porovnani = kontrola_bulls_and_cows(cisla_uzivatele, cisla_pocitace, pocet_cislic)
+
+    vytezstvi = sumarizace_vysledku(porovnani)
+print(
+    f"""Correct, you've guessed the right number in {pocet_kol} guesses!
+{oddelovac}\nThat's amazing"""
+)
